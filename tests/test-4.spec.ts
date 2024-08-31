2c467eba81a91d01cd68ba16dc6dff834e83a175
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2024-08-31 10:53:16 -0400

    state plate added

---
+++ tests/test-4.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+
+test("LicensePlateGen", async ({ page }) => {
+
+  await page.goto(
+    "https://mohan-chinnappan-n5.github.io/common/license-plate/index.html"
+  );
+  await page.getByRole("textbox").nth(1).click();
+  await page.getByRole("textbox").nth(1).fill("#78a749");
+  await page.getByRole("textbox").nth(2).click();
+  await page.getByRole("textbox").nth(2).fill("#0466c8");
+  await page.getByRole("textbox").nth(3).click();
+  await page.getByRole("textbox").nth(3).fill("#edb7b7");
+
+  await page.screenshot({ path: `license.png`, fullPage: true });
+
+});
+
+
+test("LicensePlateGenMulti", async ({ page }) => {
+
+  await page.goto(
+    "https://mohan-chinnappan-n5.github.io/common/license-plate/index.html"
+  );
+
+  const states = ['New Jersey', 'New York', 'California'];
+
+  for (const state of states) {
+    await page.getByRole('combobox').selectOption(state);
+    await page.getByRole('textbox').nth(1).click();
+    await page.getByRole('textbox').nth(1).fill('#7ad61f');
+    await page.getByRole('textbox').nth(2).click();
+    await page.getByRole('textbox').nth(2).fill('#1675d4');
+    await page.getByRole('textbox').nth(3).click();
+    await page.getByRole('textbox').nth(3).fill('#4f0808');
+    await page.screenshot({ path: `${(state).replace(' ','_')}.png`, fullPage: true });
+  }
+});
+
